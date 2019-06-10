552bd86dfa31ec4bac4168d6c19073c5c9c47584
Varvara Babkova <varvara.munday@gmail.com>
2019-06-10 18:33:11 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,9 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |style, lang|
+    
+    new_hash[lang] = {lang => {:type => lang[:type], :style => []}}
+    
+  end
 end
